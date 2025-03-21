52a35b97d4a77b10f27b946021a91f436ee1d725
Wall3n <franmoreno1506@gmail.com>
2025-03-21 10:25:34 +0100

    Fix: Implementacion de privacidad en el fichero de cabecera

--- src/divide-y-venceras.hpp
+++ src/divide-y-venceras.hpp
@@ -10,13 +10,7 @@ struct Solucion {
     int maxc;
 };
 
-bool esPequeno(int p, int q, int m);
-
-int Dividir(int p, int q);
-
 Solucion SolucionDirecta(int p, int q, int m, char c, string A);
-
-Solucion Combinar(Solucion s1, Solucion s2, int h, int m, char c, string A);
 
 Solucion DyV(int p, int q, int m, char c, string A);
 
