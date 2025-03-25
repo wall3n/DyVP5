505c8fc0c58869eaaf72dfacee31f85e3dc02f14
Wall3n <franmoreno1506@gmail.com>
2025-03-25 10:51:46 +0100

    Fix: Eliminacion del namespace en el archivo cabecera

--- src/divide-y-venceras.hpp
+++ src/divide-y-venceras.hpp
@@ -2,16 +2,15 @@
 #define __DIVIDE_Y_VENCERAS__
 
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 struct Solucion {
     int maxi;
     int maxc;
 };
 
-Solucion SolucionDirecta(int p, int q, int m, char c, const string &A);
+Solucion SolucionDirecta(int p, int q, int m, char c, const std::string &A);
 
-Solucion DyV(int p, int q, int m, char c, const string &A);
+Solucion DyV(int p, int q, int m, char c, const std::string &A);
 
 #endif
